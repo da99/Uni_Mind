796c7da80764f618e3d2ec8f3b2a6473957bf8d4
da99 <i-hate-spam-45671204@mailinator.com>
2012-01-16 19:27:23 -0600

    TEMP: expanded :upload method

--- lib/Uni_Mind/Template_Dir.rb
+++ lib/Uni_Mind/Template_Dir.rb
@@ -49,8 +49,47 @@ class Uni_Mind
         Unified_IO::Local::Dir.new(addr(raw_name))
       end
       
+      include Unified_IO::Remote::SSH::DSL
       def upload
-        files(:latest).each(&:upload)
+        # return files(:latest).each(&:upload)
+        
+        Dir.glob(File.join addr(:latest), '/*').each { |path|
+          next unless File.file?(path)
+          local = path
+          remote = '/' + File.basename( path ).gsub(',', '/') 
+          local_content = String!(File.read(local)).file_read!
+          raw_remote = begin
+                          ssh.run("[[ -f #{remote} ]] && cat #{remote}").strip
+                        rescue Unified_IO::Remote::SSH::Failed  => e
+                          ''
+                        end
+          remote_content = String!( raw_remote ).file_read!
+          
+          if !remote_content.empty? && local_content == remote_content # REMOTE EXISTS and is same?
+            # do nothing
+          elsif remote_content.empty?
+            # upload
+            puts "uploading file: #{local} => #{remote}"
+            ssh.upload local, remote
+
+          else
+            # download to origins
+            in_origins = Dir.glob(addr(:origins) + '/*').detect { |path|
+              next unless File.file?(path)
+              String!(File.read path).file_read! == remote_content
+            }
+            if in_origins
+              # do nothing
+            else
+              orig_path = File.join(addr(:origins), path + Time.now.strftime(".%F--%T").gsub(':','.'))
+              pend_path = File.join(addr(:pending), path + Time.now.strftime(".%F--%T").gsub(':','.'))
+              File.open(orig_path, 'w' ) { |io| io.write remote_content }
+              File.open(pend_path, 'w' ) { |io| io.write remote_content }
+              shell.notify "Content needs to be reviewed/merged into :latest: #{pend_path}"
+              abort
+            end
+          end
+        }
       end
 
     end # === module Base
